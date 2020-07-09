508b9b509097c64f3e128978e9567a1e0499968c
Jerry King <djerryboy2@gmail.com>
2020-07-09 19:11:01 +0200

    changed port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var options = {
     port: 3306,
     user: 'root',
     password: '5VGR6dbERV53wr9',
-    database: 'app5050',
+    database: 'fifty',
   // Whether or not to automatically check for and clear expired sessions:
   clearExpired: true,
   // How frequently expired sessions will be cleared; milliseconds:
